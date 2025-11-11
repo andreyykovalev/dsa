f8e8378c43fdf3d1774c8631bc73234053f53df9
andreyykovalev <33747452+andreyykovalev@users.noreply.github.com>
2025-11-11 08:51:47 +0100

    Time: 34 ms (31.61%) | Memory: 59 MB (10.92%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,28 @@
 class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-    //[-1,0,1,2,-1,-4]
-    //[[-1,-1,2],[-1,0,1]]
+    List<List<Integer>> res = new ArrayList<>();
     Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+    for (int i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-        for(int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            Map<Integer, Integer> map = new HashMap<>();
-            int target = -nums[i];
-
-            for(int j = i + 1; j < nums.length; j++) {
-                int complement = target - nums[j];
-
-                if(map.containsKey(complement)) {
-                    result.add(Arrays.asList(nums[i], nums[j], complement));
-                    while (j + 1 < nums.length && nums[j] == nums[j + 1]) j++;
-                }
-                map.put(nums[j], j);
+        int left = i + 1, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0) {
+                res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
             }
         }
-
-        return result;
     }
+
+    return res;
+}
 }
