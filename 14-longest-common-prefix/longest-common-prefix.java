c4a260a7ab3da7933a4849c17576735097f0c500
andreyykovalev <33747452+andreyykovalev@users.noreply.github.com>
2025-11-26 17:41:36 +0100

    Time: 1 ms (60.01%) | Memory: 43 MB (34.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,15 +3,20 @@ class Solution {
 //        Input: strs = ["flower","flow","flight"]
 //        Output: "fl"
 
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "";
+        StringBuilder sb = new StringBuilder();
+
+        String firstStr = strs[0];
+
+        for(int i = 0; i < strs[0].length(); i++) {
+            char c = firstStr.charAt(i);
+            
+            for(int j = 1; j < strs.length; j++) {
+                if(i >= strs[j].length() || c != strs[j].charAt(i)) {
+                    return sb.toString();
                 }
             }
+            sb.append(c);
         }
-        return prefix;
+        return sb.toString();
     }
 }
