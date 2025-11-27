21f0aaf7c321564195d72398d93749fb02440556
andreyykovalev <33747452+andreyykovalev@users.noreply.github.com>
2025-11-27 12:24:59 +0100

    Time: 1 ms (9.62%) | Memory: 48.5 MB (8.80%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,91 @@
+class Solution {
+
+    public int search(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
+
+
+         public int search(int[] nums, int target, int start, int end) {
+            int mid = end - start / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(start >= end) {
+                return -1;
+            }
+            ///1,2,3,4,5
+            //m = 2
+            //t = 4
+            if(nums[mid] > target) {
+                return search(nums, target, 0, mid - 1);
+            } else {
+                return search(nums, target, mid + 1, end);
+            }
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public int search(int[] nums, int target) {
+    //     return search(nums, target, 0, nums.length - 1);
+    // }
+
+    // private int search(int[] nums, int target, int left, int right) {
+    //     int mid = (left + right) / 2;
+
+    //     if(target == nums[mid]) {
+    //         return mid;
+    //     }
+    //     if(left >= right) {
+    //         return -1;
+    //     }
+    //     //targ = 10
+    //     //1,2,4,10,345
+    //     if(target > nums[mid]) {
+    //         return search(nums, target, mid + 1, right);
+    //     } else {
+    //         return search(nums, target, left, mid - 1);
+    //     }
+    // }
+
+    // public int search(int[] nums, int target) {
+    //     int left = 0;
+    //     int right = nums.length - 1;
+
+    //     while(left <= right) {
+
+    //         int mid = (left + right) / 2;
+
+    //         if(target == nums[mid]) {
+    //             return mid;
+    //         }
+
+    //         if(target > nums[mid]) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid -1;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+// }
